e0fcf1c8cc55d39606c31507245daee6c2a92b12
yuetao <yuetao@bbdservice.com>
2016-12-28 10:39:50 +0800

    vuedemo

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import mytest from './mytest.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(mytest)
+})
---
+++ src/mytest.vue
@@ -0,0 +1,59 @@
+<template>
+	<div>
+		<h1>name:{{name}}</h1>
+		<h3>age:{{age}}</h3>
+		<p>my favorite is:</p>
+		<ol>
+			<li v-for='fav in favs'>
+				{{fav}}
+			</li>
+		</ol>
+		<section v-on:mouseover='over'>	
+			<input ref='input' v-model='msg'></input>{{msg}}
+		</section>
+
+		<list v-bind:citys='citys'>
+
+		</list>
+	</div>
+</template>
+
+<script>
+	import Vue from 'vue';
+	Vue.component('list', {
+		props:['citys'],
+		template: "<ol><li v-for='city in citys'>The city name is: {{city.name}}</li></ol>"
+	});
+	export default{
+		data(){
+			return {
+				name:'yoto',
+				age:'22',
+				favs:['sing','run','swimming'],
+				msg:'hello',
+				citys:[
+					{name:'sicuan'},
+					{name:'guangdong'},
+					{name:'beijing'},
+				]
+			}
+		},
+		methods:{
+			over:function(){
+				alert('input value is:'+this.$refs.input.value);
+			}
+		}
+	}
+</script>
+
+<style>
+	h1{
+		color:red;
+	}
+	h3{
+		color:blue;
+	}
+	span{
+		color:gray;
+	}
+</style>
